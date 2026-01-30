3470aa37b8b20b85a69b7e1940322882e719dba5
tapanutch-blip <tapanut.ch@ku.th>
2026-01-30 12:01:19 +0700

    Add files via upload

---
+++ Backend/index.js
@@ -0,0 +1,14 @@
+//ทำการimportโมดูลhttp
+const http=require('http');
+const host='localhost';
+const port=8000;
+//กำหนดค่าserver
+const requestListener=function(req,res){
+    res.writeHead(200);
+    res.end('Hello, World! This is my first server.')
+}
+//run server
+const server = http.createServer(requestListener);
+server.listen(port,host,()=>{
+    console.log('Server is running on http://${host}:${port}');
+});
